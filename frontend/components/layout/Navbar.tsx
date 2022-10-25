2163928a9952fbad239bbb09ce12b740787457df
BoilingSoup <boilingsoup@protonmail.com>
2022-10-25 14:40:32 -0700

    add sign in modal

--- frontend/components/layout/Navbar.tsx
+++ frontend/components/layout/Navbar.tsx
@@ -1,5 +1,5 @@
-import { Button, Center, Navbar as MantineNavbar } from "@mantine/core";
-import React from "react";
+import { Button, Center, Modal, Navbar as MantineNavbar, Stack } from "@mantine/core";
+import React, { useState } from "react";
 import { colorSchemeHandler } from "../../theme/color-scheme-handler";
 import { NavLink } from "./NavLink";
 
@@ -7,29 +7,39 @@ type Props = {
   opened: boolean;
 };
 
-export const Navbar = ({ opened }: Props) => {
+export const Navbar = ({ opened: navbarOpened }: Props) => {
+  const [modalOpened, setModalOpened] = useState(false);
+
   return (
-    <MantineNavbar
-      hiddenBreakpoint="xl"
-      hidden={!opened}
-      width={{ sm: 200, lg: 300 }}
-      sx={(theme) => ({
-        backgroundColor: colorSchemeHandler(theme.colorScheme, {
-          light: theme.colors.blue[1],
-          dark: theme.colors.slate[9],
-        }),
-      })}
-    >
-      <NavLink href="/" text="Browse All" />
-      <NavLink text="Categories">
-        <NavLink text="test" />
-      </NavLink>
-      <NavLink href="/bookmarks" text="Bookmarks" />
-      <Center>
-        <Button href="https://google.com" component="a" sx={{ width: "85%", margin: "1rem" }}>
-          Sign In
-        </Button>
-      </Center>
-    </MantineNavbar>
+    <>
+      <Modal centered opened={modalOpened} onClose={() => setModalOpened(false)} title="Introduce yourself!">
+        <Stack>
+          <Button variant="white">Sign in with GitHub</Button>
+          <Button variant="white">Sign in with GitLab</Button>
+        </Stack>
+      </Modal>
+      <MantineNavbar
+        hiddenBreakpoint="xl"
+        hidden={!navbarOpened}
+        width={{ sm: 200, lg: 300 }}
+        sx={(theme) => ({
+          backgroundColor: colorSchemeHandler(theme.colorScheme, {
+            light: theme.colors.blue[1],
+            dark: theme.colors.slate[9],
+          }),
+        })}
+      >
+        <NavLink href="/" text="Browse All" />
+        <NavLink text="Categories">
+          <NavLink text="test" />
+        </NavLink>
+        <NavLink href="/bookmarks" text="Bookmarks" />
+        <Center>
+          <Button onClick={() => setModalOpened(true)} sx={{ width: "85%", margin: "1rem" }}>
+            Sign In
+          </Button>
+        </Center>
+      </MantineNavbar>
+    </>
   );
 };
