1e646c4bee476d41088b3fdc275be832f7473dff
BoilingSoup <boilingsoup@protonmail.com>
2022-11-03 12:14:15 -0700

    add avatar column to users table

--- backend/src/database/migrations/2014_10_12_000000_create_users_table.php
+++ backend/src/database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password')->nullable();
+            $table->string('avatar')->nullable();
             $table->string('github_id')->nullable();
             $table->string('github_token')->nullable();
             $table->string('github_refresh_token')->nullable();
