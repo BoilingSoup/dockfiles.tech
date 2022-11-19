f63db9bfdf8c4669054bf843fd35c5ce2ab284bc
BoilingSoup <boilingsoup@protonmail.com>
2022-11-18 18:12:26 -0800

    create Categories factory and seeder

--- backend/src/database/factories/CategoriesFactory.php
+++ backend/src/database/factories/CategoriesFactory.php
@@ -10,6 +10,20 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 class CategoriesFactory extends Factory
 {
     /**
+     * Get available categories
+     *
+     * @return \Illuminate\Support\Collection
+     */
+    public static function categories()
+    {
+        return collect([
+            "Web Development",
+            "Penetration Testing",
+            "Data Science"
+        ]);
+    }
+
+    /**
      * Define the model's default state.
      *
      * @return array<string, mixed>
--- backend/src/database/seeders/CategoriesSeeder.php
+++ backend/src/database/seeders/CategoriesSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Categories;
+use Database\Factories\CategoriesFactory;
 use Illuminate\Database\Seeder;
 
 class CategoriesSeeder extends Seeder
@@ -13,6 +15,8 @@ class CategoriesSeeder extends Seeder
      */
     public function run()
     {
-        //
+        CategoriesFactory::categories()->each(
+            fn ($category) => Categories::create(['name' => $category])
+        );
     }
 }
