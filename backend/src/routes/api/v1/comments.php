aa608a6fc45adb45fec6049520bcf19498dd31bd
BoilingSoup <boilingsoup@protonmail.com>
2022-12-01 09:37:48 -0800

    change comments route path

--- backend/src/routes/api/v1/comments.php
+++ backend/src/routes/api/v1/comments.php
@@ -2,8 +2,8 @@
 
 use App\Http\Controllers\CommentsController;
 
-Route::get('configuration/{configuration}/comments/', [CommentsController::class, 'index'])->name('comments.index');
+Route::get('environments/{environment}/comments/', [CommentsController::class, 'index'])->name('comments.index');
 
-Route::post('configurations/{configuration}/comments/{comment}', [CommentsController::class, 'store'])->name('comments.store');
+Route::post('environments/{environment}/comments/{comment}', [CommentsController::class, 'store'])->name('comments.store');
 
-Route::delete('configurations/{configuration}/comments/{comment}', [CommentsController::class, 'destroy'])->name('comments.destroy');
+Route::delete('environments/{environment}/comments/{comment}', [CommentsController::class, 'destroy'])->name('comments.destroy');
