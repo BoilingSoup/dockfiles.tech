e228a15b0167db29b8cf652f24a7f8e067a75afe
BoilingSoup <boilingsoup@protonmail.com>
2022-12-02 05:06:42 -0800

    update navbar

--- frontend/components/layout/Navbar.tsx
+++ frontend/components/layout/Navbar.tsx
@@ -1,10 +1,24 @@
-import { Button, Center, CSSObject, MantineTheme, Modal, Navbar as MantineNavbar, Stack, Text } from "@mantine/core";
+import {
+  Button,
+  Center,
+  CSSObject,
+  MantineTheme,
+  Modal,
+  Navbar as MantineNavbar,
+  Stack,
+  Text,
+} from "@mantine/core";
 import { IconBrandGithub, IconBrandGitlab } from "@tabler/icons";
 import React, { useState } from "react";
 import { colorSchemeHandler } from "../../theme/color-scheme-handler";
 import { NavLink } from "./NavLink";
 
-const signInBtnSx = ({ colors, colorScheme, fn, shadows }: MantineTheme): CSSObject => ({
+const signInBtnSx = ({
+  colors,
+  colorScheme,
+  fn,
+  shadows,
+}: MantineTheme): CSSObject => ({
   width: "85%",
   margin: "1rem",
   boxShadow: shadows.xl,
@@ -37,7 +51,15 @@ const oAuthBtnSx = ({ colors, colorScheme, fn }: MantineTheme): CSSObject => ({
 
 const iconSize = 30;
 
-type modalStylesApi = "title" | "body" | "header" | "root" | "overlay" | "modal" | "close" | "inner";
+type modalStylesApi =
+  | "title"
+  | "body"
+  | "header"
+  | "root"
+  | "overlay"
+  | "modal"
+  | "close"
+  | "inner";
 type modalStylesObj = { [key in modalStylesApi]?: CSSObject };
 
 const modalStyles: modalStylesObj = {
@@ -56,22 +78,39 @@ export const Navbar = ({ opened: navbarOpened }: Props) => {
 
   return (
     <>
-      <Modal centered opened={modalOpened} onClose={() => setModalOpened(false)} title="Sign In" styles={modalStyles}>
+      <Modal
+        centered
+        opened={modalOpened}
+        onClose={() => setModalOpened(false)}
+        title="Sign In"
+        styles={modalStyles}
+      >
         <Stack>
-          <Button variant="white" sx={oAuthBtnSx} leftIcon={<IconBrandGithub size={iconSize} />}>
+          <Button
+            variant="white"
+            sx={oAuthBtnSx}
+            leftIcon={<IconBrandGithub size={iconSize} />}
+          >
             <Text size="lg">Sign in with GitHub</Text>
           </Button>
-          <Button variant="white" sx={oAuthBtnSx} leftIcon={<IconBrandGitlab size={iconSize} />}>
+          <Button
+            variant="white"
+            sx={oAuthBtnSx}
+            leftIcon={<IconBrandGitlab size={iconSize} />}
+          >
             <Text size="lg">Sign in with GitLab</Text>
           </Button>
         </Stack>
       </Modal>
-      <MantineNavbar hiddenBreakpoint="xl" hidden={!navbarOpened} width={{ sm: 200, lg: 300 }} sx={navbarSx}>
-        <NavLink href="/" text="Browse All" />
-        <NavLink text="Categories">
-          <NavLink text="test" />
-        </NavLink>
+      <MantineNavbar
+        hiddenBreakpoint="xl"
+        hidden={!navbarOpened}
+        width={{ sm: 200, lg: 300 }}
+        sx={navbarSx}
+      >
+        <NavLink href="/" text="Browse" />
         <NavLink href="/bookmarks" text="Bookmarks" />
+        <NavLink href="/notifications" text="Notifications" />
         <Center>
           <Button onClick={() => setModalOpened(true)} sx={signInBtnSx}>
             Sign In
