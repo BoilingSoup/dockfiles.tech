5057c324a0947da30124b91d51171642402f9bb3
BoilingSoup <boilingsoup@protonmail.com>
2022-12-02 05:08:09 -0800

    add Notifications page

---
+++ frontend/pages/notifications/index.tsx
@@ -0,0 +1,9 @@
+import { Text } from "@mantine/core";
+import { NextPage } from "next";
+import React from "react";
+
+const Notifications: NextPage = () => {
+  return <Text component="h1">Notifications</Text>;
+};
+
+export default Notifications;
