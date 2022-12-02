38728950e1440cccd290594048eca7feefbb27d5
BoilingSoup <boilingsoup@protonmail.com>
2022-12-02 10:14:20 -0800

    style HomeSearch component

--- frontend/components/common/Select.tsx
+++ frontend/components/common/Select.tsx
@@ -3,16 +3,38 @@ import { IconChevronDown } from "@tabler/icons";
 
 type Props = {
   data: string[] | undefined;
+  ml?: number;
 };
 
-export const Select = ({ data }: Props) => {
+/**
+ * Pre-populate options with default option.
+ * Append options retrieved from server when data is available.
+ **/
+const getOptions = (
+  defaultOption: string,
+  data: string[] | undefined
+): string[] => {
+  let options: string[] = [];
+
+  if (data !== undefined) {
+    options = [defaultOption, ...data];
+  } else {
+    options.push(defaultOption);
+  }
+
+  return options;
+};
+
+export const Select = ({ data, ml = 12 }: Props) => {
+  const defaultOption = "All Categories";
+  const options = getOptions(defaultOption, data);
+
   return (
     <NativeSelect
-      label="Category"
-      placeholder="Select Category"
-      data={data ?? [""]}
+      data={options}
       rightSection={<IconChevronDown size={14} />}
       rightSectionWidth={40}
+      ml={ml}
     />
   );
 };
