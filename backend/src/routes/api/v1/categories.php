85fdffb92757ad0b073f9ff0eb177d9af4642198
BoilingSoup <boilingsoup@protonmail.com>
2022-12-03 14:06:07 -0800

    update route path

--- backend/src/routes/api/v1/categories.php
+++ backend/src/routes/api/v1/categories.php
@@ -3,8 +3,8 @@
 use App\Http\Controllers\CategoriesController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('categories/list', [CategoriesController::class, 'index'])->name('categories.index');
+Route::get('categories', [CategoriesController::class, 'index'])->name('categories.index');
 
-Route::get('categories/{id}', [CategoriesController::class, 'show'])
+Route::get('categories/{id}/environments', [CategoriesController::class, 'show'])
 ->whereNumber('id')
 ->name('categories.show');
