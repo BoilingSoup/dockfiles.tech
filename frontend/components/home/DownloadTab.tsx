c7b4ab70bb6ce39d7a26f4eb9ddf7ba12a37adc2
BoilingSoup <boilingsoup@protonmail.com>
2022-12-08 07:12:02 -0800

    style download tab

--- frontend/components/home/DownloadTab.tsx
+++ frontend/components/home/DownloadTab.tsx
@@ -1,23 +1,35 @@
+import { Button, Center, Container, Divider, Group, Text } from "@mantine/core";
+import { IconArrowRight } from "@tabler/icons";
 import { Props as EnvironmentProps } from "../../pages/[string_id]";
 import { CodeBlock } from "./_codeBlock";
 
 export const DownloadTab = ({ environment }: EnvironmentProps) => {
+  const directLink = `https://github.com/${environment.repo_owner}/${environment.repo_name}/archive/${environment.repo_branch}.zip`;
+
   return (
     <>
-      <CodeBlock
-        title="Wget"
-        code={`wget https://github.com/${environment.repo_owner}/${environment.repo_name}/archive/${environment.repo_branch}.zip`}
-      />
+      <Container>
+        <CodeBlock
+          title="Git clone"
+          code={`git clone https://github.com/${environment.repo_owner}/${environment.repo_name}`}
+        />
+        <Divider />
 
-      <CodeBlock
-        title="Curl"
-        code={`curl -L -O https://github.com/${environment.repo_owner}/${environment.repo_name}/archive/${environment.repo_branch}.zip`}
-      />
+        <CodeBlock title="Curl" code={`curl -L -O ${directLink}`} />
+        <Divider />
 
-      <CodeBlock
-        title="Git clone"
-        code={`git clone https://github.com/${environment.repo_owner}/${environment.repo_name}`}
-      />
+        <CodeBlock title="Wget" code={`wget ${directLink}`} />
+        <Divider />
+
+        <Center mt={46} style={{ display: "flex", alignItems: "center" }}>
+          <Text component="h3" style={{ fontSize: "2rem", display: "inline" }}>
+            or Download zip
+          </Text>
+          <Button size="md" ml={50}>
+            Download
+          </Button>
+        </Center>
+      </Container>
     </>
   );
 };
