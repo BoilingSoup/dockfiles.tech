b88e52e9d46700b9f38fb015015c580d708aef89
BoilingSoup <boilingsoup@protonmail.com>
2022-12-31 06:31:37 -0800

    add avatar round bordder, improve comments content margins

--- frontend/components/details/styles.ts
+++ frontend/components/details/styles.ts
@@ -1,35 +1,44 @@
 import { CSSObject, MantineTheme } from "@mantine/core";
 import { colorSchemeHandler } from "../../theme/color-scheme-handler";
 
+const nameAreaHeight = 70;
+const avatarContainerWidth = 70;
+
 export const paperSx = ({ colors, colorScheme }: MantineTheme): CSSObject => ({
-  background: colorSchemeHandler(colorScheme, { light: colors.blue[0], dark: colors.slate[8] }),
-  // "&:hover": {
-  //   background: colorSchemeHandler(colorScheme, { light: colors.cyan[2], dark: colors.slate[6] }),
-  // },
+  background: colorSchemeHandler(colorScheme, {
+    light: colors.blue[0],
+    dark: colors.slate[8],
+  }),
   margin: 14,
 });
 
-export const boxSx = ({ colors, colorScheme }: MantineTheme): CSSObject => ({
+export const boxSx = (): CSSObject => ({
   display: "flex",
-  height: 50,
+  alignItems: "center",
+  height: nameAreaHeight,
   width: "100%",
 });
 
 export const avatarSx = ({ colors, colorScheme, radius }: MantineTheme): CSSObject => ({
   borderRadius: radius.xl,
-  border: "1px solid white",
+  border: colorSchemeHandler(colorScheme, {
+    light: `1px solid ${colors.blue[8]}`,
+    dark: `1px solid ${colors.blue[6]}`,
+  }),
 });
 
-export const avatarGroupSx = ({ colors, colorScheme, radius }: MantineTheme): CSSObject => ({
+export const avatarGroupSx = (): CSSObject => ({
   justifyContent: "center",
-  width: 70,
+  width: avatarContainerWidth,
 });
 
-export const nameSx = ({ colors, colorScheme, radius }: MantineTheme): CSSObject => ({
+export const nameSx = (): CSSObject => ({
   fontSize: "1.2rem",
 });
 
-export const contentSx = ({ colors, colorScheme, radius }: MantineTheme): CSSObject => ({
-  marginLeft: 70,
+export const contentSx = (): CSSObject => ({
+  marginTop: 0,
+  marginBottom: 0,
+  marginLeft: avatarContainerWidth,
   paddingBottom: 10,
 });
