5f4245e7dff382ad59ad137f87fb57bd374f4d49
BoilingSoup <boilingsoup@protonmail.com>
2023-01-03 12:46:01 -0800

    filter columns in bookmarks index query

--- backend/src/app/Repositories/BookmarksRepository.php
+++ backend/src/app/Repositories/BookmarksRepository.php
@@ -4,6 +4,7 @@ namespace App\Repositories;
 
 use App\Helpers\Cache\CACHE_KEYS;
 use App\Helpers\Cache\CACHE_TAGS;
+use App\Models\Environments;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -21,7 +22,12 @@ class BookmarksRepository
         $userId = Auth::user()->id;
         return Cache::tags([CACHE_TAGS::USER_BOOKMARKS_($userId), CACHE_TAGS::USER_BOOKMARKS_INDEX])->rememberForever(
             CACHE_KEYS::USER_BOOKMARKS_INDEX_CURSOR_($userId, $request->cursor),
-            fn () => Auth::user()->bookmarks()->orderByPivot(Model::CREATED_AT)->cursorPaginate()
+            function () {
+                return Environments::select("id", "name", "string_id")
+                  ->withCount("comments")
+                  ->whereIn("id", Auth::user()->bookmarks()->allRelatedIds())
+                  ->cursorPaginate();
+            }
         );
     }
 }
