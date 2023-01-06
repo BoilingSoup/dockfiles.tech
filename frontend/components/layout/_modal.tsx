077178b6d17585761d86ff70c9b99ab6e7ee1ab8
BoilingSoup <boilingsoup@protonmail.com>
2023-01-05 16:34:43 -0800

    add form to modal

--- frontend/components/layout/_modal.tsx
+++ frontend/components/layout/_modal.tsx
@@ -1,5 +1,7 @@
-import { Button, Modal as MantineModal, Stack, Text } from "@mantine/core";
+import { Button, Checkbox, Group, Input, Modal as MantineModal, Stack, Text, TextInput } from "@mantine/core";
+import { useForm } from "@mantine/form";
 import { IconBrandGithub, IconBrandGitlab } from "@tabler/icons";
+import { Divider } from "../common/Divider";
 import { modalStyles, oAuthBtnSx } from "./styles";
 
 const iconSize = 30;
@@ -10,9 +12,33 @@ type Props = {
 };
 
 export const Modal = ({ opened, onClose: modalCloseHandler }: Props) => {
+  const form = useForm({
+    initialValues: {
+      email: "",
+      password: "",
+      termsOfService: false,
+    },
+
+    validate: {
+      email: (value) => (/^\S+@\S+$/.test(value) ? null : "Invalid email"),
+      password: (value) => (value.length >= 8 ? null : "Password must be at least 8 characters long"),
+    },
+  });
+
   return (
     <MantineModal centered opened={opened} onClose={modalCloseHandler} title="Sign In" styles={modalStyles}>
       <Stack>
+        <form onSubmit={form.onSubmit((values) => console.log(values))}>
+          <TextInput label="Email" placeholder="your@email.com" {...form.getInputProps("email")} />
+          <TextInput label="Password" {...form.getInputProps("password")} />
+          <Checkbox mt="md" label="Remember me" {...form.getInputProps("termsOfService", { type: "checkbox" })} />
+
+          <Group position="right" mt="md">
+            <Button type="submit">Submit</Button>
+          </Group>
+        </form>
+
+        <Divider />
         <Button variant="white" sx={oAuthBtnSx} leftIcon={<IconBrandGithub size={iconSize} />}>
           <Text size="lg">Sign in with GitHub</Text>
         </Button>
