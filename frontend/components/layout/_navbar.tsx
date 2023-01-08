71513fd0bc2cb1756475fe841cac038978ed6fc4
BoilingSoup <boilingsoup@protonmail.com>
2023-01-07 17:56:52 -0800

    move logout button to separate component

--- frontend/components/layout/_navbar.tsx
+++ frontend/components/layout/_navbar.tsx
@@ -8,8 +8,6 @@ import { RegisterModal } from "./_registerModal";
 import { useAuth } from "../../contexts/AuthProvider";
 import { useLogoutMutation } from "../../hooks/api/useLogoutMutation";
 
-export const navBarHiddenBreakPoint = "sm";
-
 type UnauthenticatedBtnProps = {
   onLoginClick: () => void;
   onRegisterClick: () => void;
@@ -31,14 +29,24 @@ const UnauthenticatedButtons = ({
   );
 };
 
+const LogoutButton = () => {
+  const { mutate: logoutMutation, isLoading: logoutIsLoading } = useLogoutMutation();
+  return (
+    <Button onClick={() => logoutMutation()} sx={authControlBtnSx}>
+      {logoutIsLoading ? <Loader color="gray" size="sm" /> : "Sign out"}
+    </Button>
+  );
+};
+
 type Props = {
   opened: boolean;
   onLinkClick: () => void;
 };
 
+export const navBarHiddenBreakPoint = "sm";
+
 export const Navbar = ({ opened: navbarOpened, onLinkClick: navbarCloseHandler }: Props) => {
   const { user } = useAuth();
-  const { mutate: logoutMutation, isLoading: logoutIsLoading } = useLogoutMutation();
 
   const [loginModalOpened, setLoginModalOpened] = useState(false);
   const [registerModalOpened, setRegisterModalOpened] = useState(false);
@@ -64,9 +72,7 @@ export const Navbar = ({ opened: navbarOpened, onLinkClick: navbarCloseHandler }
         {user && <UserNavLinksGroup onLinkClick={navbarCloseHandler} />}
         <Center style={{ flexDirection: "column" }}>
           {user ? (
-            <Button onClick={() => logoutMutation()} sx={authControlBtnSx}>
-              {logoutIsLoading ? <Loader color="gray" size="sm" /> : "Sign out"}
-            </Button>
+            <LogoutButton />
           ) : (
             <UnauthenticatedButtons onLoginClick={loginModalOpenHandler} onRegisterClick={registerModalOpenHandler} />
           )}
