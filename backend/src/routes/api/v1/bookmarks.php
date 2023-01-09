0c104923afa8a26ee68f33db438c3b4d4cd71e78
BoilingSoup <boilingsoup@protonmail.com>
2023-01-09 07:00:01 -0800

    update bookmarks route URIs

--- backend/src/routes/api/v1/bookmarks.php
+++ backend/src/routes/api/v1/bookmarks.php
@@ -4,9 +4,9 @@ use App\Http\Controllers\BookmarksController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware("auth:sanctum")->name("bookmarks.")->group(function () {
-    Route::get("bookmarks", [BookmarksController::class, "index"])->name("index");
+    Route::get("bookmarks/environments", [BookmarksController::class, "index"])->name("index");
 
-    Route::post("bookmarks", [BookmarksController::class, "store"])->name("store");
+    Route::post("bookmarks/environments", [BookmarksController::class, "store"])->name("store");
 
-    Route::delete("bookmarks", [BookmarksController::class, "destroy"])->name("destroy");
+    Route::delete("bookmarks/environments", [BookmarksController::class, "destroy"])->name("destroy");
 });
