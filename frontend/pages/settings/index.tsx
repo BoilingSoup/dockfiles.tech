17f955d5495421e97e02097aa57d317c1bdde34f
BoilingSoup <boilingsoup@protonmail.com>
2023-01-09 18:03:38 -0800

    redirect settings page to home page if unauthenticated

--- frontend/pages/settings/index.tsx
+++ frontend/pages/settings/index.tsx
@@ -3,8 +3,10 @@ import { NextPage } from "next";
 import Head from "next/head";
 import React from "react";
 import { SITE_NAME } from "../../config/config";
+import { useRedirectUnauthenticated } from "../../hooks/helpers/useRedirectUnauthenticated";
 
 const Settings: NextPage = () => {
+  useRedirectUnauthenticated("/");
   return (
     <>
       <Head>
