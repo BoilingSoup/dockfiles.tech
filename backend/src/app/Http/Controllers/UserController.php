760bfd0e3a856438d77ac08493d905f56f60fe91
BoilingSoup <boilingsoup@protonmail.com>
2023-01-12 04:27:21 -0800

    user can change name

--- backend/src/app/Http/Controllers/UserController.php
+++ backend/src/app/Http/Controllers/UserController.php
@@ -2,6 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\UpdateUserRequest;
+use Illuminate\Support\Facades\Auth;
+
 class UserController extends Controller
 {
+    public function update(UpdateUserRequest $request)
+    {
+        $validated = $request->validated();
+
+        Auth::user()->name = $validated["name"];
+        Auth::user()->saveOrFail();
+        return Auth::user();
+    }
 }
---
+++ backend/src/app/Http/Requests/UpdateUserRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
+
+class UpdateUserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return Auth::check();
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+          'name' => ['required_without:email', 'string', 'max:20'],
+          'email' => ['required_without:name', 'string', 'email', 'max:30'],
+        ];
+    }
+}
