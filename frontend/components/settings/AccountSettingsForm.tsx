b37fe90c1c375f3fe1fdba472a45fc46c17d338e
BoilingSoup <boilingsoup@protonmail.com>
2023-01-12 09:10:14 -0800

    move settings form to separate component

---
+++ frontend/components/settings/AccountSettingsForm.tsx
@@ -0,0 +1,60 @@
+import { Badge, Button, Group, TextInput } from "@mantine/core";
+import { useAuth } from "../../contexts/AuthProvider";
+import { formInputStyles } from "../layout/styles";
+import { buttonsSx, formMaxWidth, formWidth } from "./styles";
+
+export const AccountSettingsForm = () => {
+  const { user } = useAuth();
+
+  if (user === null) {
+    return <></>;
+  }
+
+  const emailIsVerified = user.email_verified_at !== null;
+  const isGitHub = user.github_id;
+  const isGitLab = user.gitlab_id;
+  const isOAuth = Boolean(isGitHub || isGitLab);
+  let emailValue: string;
+
+  if (isGitHub) {
+    emailValue = "GitHub Account";
+  } else if (isGitLab) {
+    emailValue = "GitLab Account";
+  } else {
+    emailValue = user.email;
+  }
+
+  const badge = emailIsVerified ? (
+    <Badge variant="gradient" gradient={{ from: "teal", to: "lime", deg: 105 }}>
+      Verified
+    </Badge>
+  ) : (
+    <Badge styles={{ root: { background: "#fcba03", color: "navy" } }}>Unverified</Badge>
+  );
+
+  return (
+    user && (
+      <div style={{ maxWidth: formMaxWidth, width: formWidth, margin: "auto", marginTop: "30px" }}>
+        <TextInput styles={formInputStyles} mt="lg" label="Display Name" placeholder="Name" value={user.name} />
+        <TextInput
+          styles={formInputStyles}
+          mt="lg"
+          label="Email"
+          placeholder="Email"
+          value={emailValue}
+          disabled={!emailIsVerified || isOAuth}
+          rightSection={badge}
+          rightSectionWidth={100}
+        />
+        <Group mt="xl">
+          <Button sx={buttonsSx} mt="lg" disabled={emailIsVerified}>
+            Resend Verification Email
+          </Button>
+          <Button sx={buttonsSx} mt="lg" ml="auto">
+            Save Changes
+          </Button>
+        </Group>
+      </div>
+    )
+  );
+};
--- frontend/pages/settings/index.tsx
+++ frontend/pages/settings/index.tsx
@@ -1,13 +1,13 @@
-import { Badge, Button, Center, Container, Group, Text, TextInput } from "@mantine/core";
+import { Center, Container, Text } from "@mantine/core";
 import { NextPage } from "next";
 import Head from "next/head";
 import React from "react";
 import { Divider } from "../../components/common/Divider";
 import { mainContainerSx } from "../../components/common/styles";
-import { formInputStyles } from "../../components/layout/styles";
+import { AccountSettingsForm } from "../../components/settings/AccountSettingsForm";
 import { Avatar } from "../../components/settings/Avatar";
 import { DeleteAccountAccordion } from "../../components/settings/DeleteAccountAccordion";
-import { buttonsSx, formMaxWidth, formWidth, titleTextSx } from "../../components/settings/styles";
+import { formMaxWidth, formWidth, titleTextSx } from "../../components/settings/styles";
 import { SITE_NAME } from "../../config/config";
 import { useAuth } from "../../contexts/AuthProvider";
 import { useRedirectUnauthenticated } from "../../hooks/helpers/useRedirectUnauthenticated";
@@ -15,8 +15,6 @@ import { useRedirectUnauthenticated } from "../../hooks/helpers/useRedirectUnaut
 const Settings: NextPage = () => {
   useRedirectUnauthenticated("/");
   const { user } = useAuth();
-
-  const emailIsVerified = user?.email_verified_at !== null;
 
   return (
     <>
@@ -33,35 +31,7 @@ const Settings: NextPage = () => {
           </Center>
           <Center style={{ justifyContent: "space-around", flexDirection: "column" }}>
             <Avatar mt={50} />
-            <div style={{ maxWidth: formMaxWidth, width: formWidth, margin: "auto", marginTop: "30px" }}>
-              <TextInput styles={formInputStyles} mt="lg" label="Display Name" placeholder="Name" value={user.name} />
-              <TextInput
-                styles={formInputStyles}
-                mt="lg"
-                label="Email"
-                placeholder="Email"
-                value={Boolean(user.github_id || user.gitlab_id) ? "GitHub/GitLab account" : user.email}
-                disabled={!emailIsVerified || Boolean(user.github_id || user.gitlab_id)}
-                rightSection={
-                  emailIsVerified ? (
-                    <Badge variant="gradient" gradient={{ from: "teal", to: "lime", deg: 105 }}>
-                      Verified
-                    </Badge>
-                  ) : (
-                    <Badge styles={{ root: { background: "#fcba03", color: "navy" } }}>Unverified</Badge>
-                  )
-                }
-                rightSectionWidth={100}
-              />
-              <Group mt="xl">
-                <Button sx={buttonsSx} mt="lg" disabled={emailIsVerified}>
-                  Resend Verification Email
-                </Button>
-                <Button sx={buttonsSx} mt="lg" ml="auto">
-                  Save Changes
-                </Button>
-              </Group>
-            </div>
+            <AccountSettingsForm />
           </Center>
           <Divider mt={30} size="xl" />
           <Center style={{ alignItems: "center", maxWidth: formMaxWidth, width: formWidth, margin: "auto" }}>
