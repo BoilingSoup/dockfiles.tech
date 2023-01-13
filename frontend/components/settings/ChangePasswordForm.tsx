a07c576f71b24ede36b47a23b5330facd54bcbcc
BoilingSoup <boilingsoup@protonmail.com>
2023-01-13 07:45:50 -0800

    change password form placeholder values

--- frontend/components/settings/ChangePasswordForm.tsx
+++ frontend/components/settings/ChangePasswordForm.tsx
@@ -1,17 +1,58 @@
 import { Button, Center, Group, Text, TextInput } from "@mantine/core";
+import { useAuth } from "../../contexts/AuthProvider";
 import { formInputStyles } from "../layout/styles";
 import { formCenterStyles, formStyles } from "./styles";
 
 export const ChangePasswordForm = () => {
+  const { user } = useAuth();
+
+  if (user === null) {
+    return <></>;
+  }
+
+  const isGitHub = user.github_id !== null;
+  const isGitLab = user.gitlab_id !== null;
+  const isOAuth = Boolean(isGitHub || isGitLab);
+
+  let placeholderValue: string;
+  if (isGitHub) {
+    placeholderValue = "GitHub Account";
+  } else if (isGitLab) {
+    placeholderValue = "GitLab Account";
+  } else {
+    placeholderValue = "";
+  }
+
   return (
     <Center style={formCenterStyles}>
       <form style={formStyles}>
         <Text component="h2" mr="auto" my={0} style={{ fontSize: "1.3rem" }}>
           Change Password
         </Text>
-        <TextInput type="password" mt="lg" label="Old Password" styles={formInputStyles} />
-        <TextInput type="password" mt="lg" label="New Password" styles={formInputStyles} />
-        <TextInput type="password" mt="lg" label="Confirm New Password" styles={formInputStyles} />
+        <TextInput
+          type={isOAuth ? "text" : "password"}
+          disabled={isOAuth}
+          mt="lg"
+          label="Old Password"
+          value={placeholderValue}
+          styles={formInputStyles}
+        />
+        <TextInput
+          type={isOAuth ? "text" : "password"}
+          disabled={isOAuth}
+          mt="lg"
+          label="New Password"
+          value={placeholderValue}
+          styles={formInputStyles}
+        />
+        <TextInput
+          type={isOAuth ? "text" : "password"}
+          disabled={isOAuth}
+          mt="lg"
+          label="Confirm New Password"
+          value={placeholderValue}
+          styles={formInputStyles}
+        />
         <Group>
           <Button mt="lg" ml="auto">
             Change Password
