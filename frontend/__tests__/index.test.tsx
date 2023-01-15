a659ef8007e14744e28e6941f97d7cafa17b8b4e
BoilingSoup <boilingsoup@protonmail.com>
2023-01-14 18:31:07 -0800

    add jest and testing library

---
+++ frontend/__tests__/index.test.tsx
@@ -0,0 +1,11 @@
+// __tests__/index.test.jsx
+
+import { render, screen } from "@testing-library/react";
+import Home from "../pages/index";
+import "@testing-library/jest-dom";
+
+describe("Home", () => {
+  it("renders a heading", () => {
+    render(<div />);
+  });
+});
---
+++ frontend/jest.config.js
@@ -0,0 +1,27 @@
+// jest.config.js
+const nextJest = require("next/jest");
+
+const createJestConfig = nextJest({
+  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
+  dir: "./",
+});
+
+const esModules = ["ky"];
+// Add any custom config to be passed to Jest
+/** @type {import('jest').Config} */
+const customJestConfig = {
+  // Add more setup options before each test is run
+  // setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  // if using TypeScript with a baseUrl set to the root directory then you need the below for alias' to work
+  moduleDirectories: ["node_modules", "<rootDir>/"],
+  testEnvironment: "jest-environment-jsdom",
+  transformIgnorePatterns: [`/node_modules/(?!(${esModules.join("|")})/)`],
+};
+
+module.exports = async () => {
+  const jestConfig = await createJestConfig(customJestConfig)();
+  return {
+    ...jestConfig,
+    transformIgnorePatterns: jestConfig.transformIgnorePatterns.filter((ptn) => ptn !== "/node_modules/"), // ['^.+\\.module\\.(css|sass|scss)$', '/node_modules/(?!(package1|package2)/']
+  };
+};
