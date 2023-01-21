dc2e8e6d2aee9439b30281c053f28fbf3556efbf
BoilingSoup <boilingsoup@protonmail.com>
2023-01-20 16:06:01 -0800

    prefetch bookmarks after successful bookmarks mutation

--- frontend/hooks/api/useBookmarkMutation.tsx
+++ frontend/hooks/api/useBookmarkMutation.tsx
@@ -1,5 +1,6 @@
 import { useMutation, useQueryClient } from "react-query";
 import { queryKeys } from "../../query-client/constants";
+import { useBookmarksCategoriesSearch } from "../../zustand-store/bookmarks/useBookmarksCategoriesSearch";
 import { useBookmarksPageCursor } from "../../zustand-store/bookmarks/useBookmarksPageCursor";
 import { INITIAL_PAGE_CURSOR } from "../../zustand-store/types";
 import {
@@ -7,14 +8,18 @@ import {
   AttemptBookmarkMetadata,
   genericErrorNotification,
   EnvironmentUserStatus,
+  getBookmarks,
 } from "./helpers";
 
 export const useBookmarkMutation = (param: AttemptBookmarkMetadata) => {
   const queryClient = useQueryClient();
+
   const { setCursor } = useBookmarksPageCursor();
+  const { input: searchParam, select: categoryId } = useBookmarksCategoriesSearch();
 
   return useMutation(() => attemptToggleBookmark(param), {
     onSuccess: () => {
+      // Update bookmarks/likes button state
       queryClient.setQueryData<EnvironmentUserStatus>(queryKeys.bookmarkLikeStatus(param.id), {
         success: true,
         data: {
@@ -22,8 +27,23 @@ export const useBookmarkMutation = (param: AttemptBookmarkMetadata) => {
           is_liked: param!.data!.data.is_liked,
         },
       });
+
+      // Delete bookmarks data from the query cache. This prevents the user from seeing a stale bookmark
+      // on the bookmarks index page.
+      // 
+      // Bookmarks/likes button state will be refetched, but will not show a spinner because its options { keepPreviousData: true }
       queryClient.removeQueries(queryKeys.bookmarks);
+
+      // Reset bookmarks index page pagination
       setCursor(INITIAL_PAGE_CURSOR);
+
+      // Prefetch bookmarks index page
+      queryClient.prefetchQuery(
+        [queryKeys.bookmarks, categoryId, queryKeys.searchStrToKey(searchParam), INITIAL_PAGE_CURSOR],
+        getBookmarks({ categoryId, cursor: INITIAL_PAGE_CURSOR, searchParam })
+      );
+
+      // NOTE: if user navigates to bookmarks index page before the prefetch is complete, the user sees a loading spinner due to queryClient.removeQueries()
     },
     onError: () => {
       genericErrorNotification();
