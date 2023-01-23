25fefacde69ef23a5064c4e8031a02a2d200fce1
BoilingSoup <boilingsoup@protonmail.com>
2023-01-23 15:57:23 -0800

    refactor

--- frontend/hooks/api/useLikeMutation.tsx
+++ frontend/hooks/api/useLikeMutation.tsx
@@ -21,17 +21,60 @@ export const useLikeMutation = (param: AttemptToggleActionMetadata) => {
 
   const { setCursor: setHomePageCursor } = useHomePageCursor();
   const { setCursor: setBookmarksPageCursor } = useBookmarksPageCursor();
-  const {
-    input: homeSearchParam,
-    // setInput: setHomeSearchParam,
-    select: homeCategoryId,
-    // setSelect: setHomeCategoryId,
-  } = useHomeCategoriesSearch();
+  const { input: homeSearchParam, select: homeCategoryId } = useHomeCategoriesSearch();
   const { input: bookmarksSearchParam, select: bookmarksCategoryId } = useBookmarksCategoriesSearch();
 
+  const resetPageCursors = () => {
+    setBookmarksPageCursor(INITIAL_PAGE_CURSOR);
+    setHomePageCursor(INITIAL_PAGE_CURSOR);
+  };
+
+  const removeStaleCacheData = () => {
+    queryClient.removeQueries(queryKeys.bookmarks);
+    queryClient.removeQueries(queryKeys.environments);
+  };
+
+  const prefetchData = () => {
+    prefetchBookmarksPage();
+    prefetchEnvironmentsPage();
+    prefetchInitialHomePageAndSetCookie();
+  };
+
+  const prefetchBookmarksPage = () => {
+    queryClient.prefetchQuery(
+      [queryKeys.bookmarks, bookmarksCategoryId, queryKeys.searchStrToKey(bookmarksSearchParam), INITIAL_PAGE_CURSOR],
+      getBookmarks({
+        categoryId: bookmarksCategoryId,
+        cursor: INITIAL_PAGE_CURSOR,
+        searchParam: bookmarksSearchParam,
+      })
+    );
+  };
+
+  const prefetchEnvironmentsPage = () => {
+    queryClient.prefetchQuery(
+      [queryKeys.environments, homeCategoryId, queryKeys.searchStrToKey(homeSearchParam), INITIAL_PAGE_CURSOR],
+      getEnvironments({
+        categoryId: homeCategoryId,
+        cursor: INITIAL_PAGE_CURSOR,
+        searchParam: homeSearchParam,
+      })
+    );
+  };
+
+  const prefetchInitialHomePageAndSetCookie = async () => {
+    const homePageData = await getEnvironments({
+      categoryId: ALL_CATEGORIES,
+      cursor: INITIAL_PAGE_CURSOR,
+      searchParam: "",
+    })();
+    queryClient.setQueryData(queryKeys.initialHomePageQueryKey, homePageData);
+    // Need to update the home page data in cookie because it's used for SSR & reruns per page navigation due to getInitialProps
+    setCookie(ENVIRONMENTS_INDEX_COOKIE_KEY, JSON.stringify(homePageData));
+  };
+
   return useMutation(() => attemptToggleLike(param), {
-    onSuccess: async () => {
-      // Update bookmarks/likes button state
+    onSuccess: () => {
       queryClient.setQueryData<EnvironmentUserStatus>(queryKeys.bookmarkLikeStatus(param.id), {
         success: true,
         data: {
@@ -42,47 +85,9 @@ export const useLikeMutation = (param: AttemptToggleActionMetadata) => {
         },
       });
 
-      // Reset pagination cursors
-      setBookmarksPageCursor(INITIAL_PAGE_CURSOR);
-      setHomePageCursor(INITIAL_PAGE_CURSOR);
-      // setHomeSearchParam("");
-      // setHomeCategoryId(ALL_CATEGORIES);
-
-      // Delete bookmarks and environments data from the query cache.
-      // Bookmarks/likes button state will be refetched, but will not show a spinner because its options { keepPreviousData: true }
-      queryClient.removeQueries(queryKeys.bookmarks);
-      queryClient.removeQueries(queryKeys.environments);
-      // queryClient.invalidateQueries(queryKeys.environments);
-
-      // Prefetch Bookmarks index page
-      queryClient.prefetchQuery(
-        [queryKeys.bookmarks, bookmarksCategoryId, queryKeys.searchStrToKey(bookmarksSearchParam), INITIAL_PAGE_CURSOR],
-        getBookmarks({
-          categoryId: bookmarksCategoryId,
-          cursor: INITIAL_PAGE_CURSOR,
-          searchParam: bookmarksSearchParam,
-        })
-      );
-      queryClient.prefetchQuery(
-        [queryKeys.environments, homeCategoryId, queryKeys.searchStrToKey(homeSearchParam), INITIAL_PAGE_CURSOR],
-        getEnvironments({
-          categoryId: homeCategoryId,
-          cursor: INITIAL_PAGE_CURSOR,
-          searchParam: homeSearchParam,
-        })
-      );
-
-      // Prefetch Home page data.
-      // Instead of prefetchQuery method, I fetch the data manually so I can also set the result in the cookie.
-      // This is necessary because getInitialProps reads from the cookie for home page data.
-      const homePageData = await getEnvironments({
-        categoryId: ALL_CATEGORIES,
-        cursor: INITIAL_PAGE_CURSOR,
-        searchParam: "",
-      })();
-
-      setCookie(ENVIRONMENTS_INDEX_COOKIE_KEY, JSON.stringify(homePageData));
-      queryClient.setQueryData(queryKeys.initialHomePageQueryKey, homePageData);
+      resetPageCursors();
+      removeStaleCacheData();
+      prefetchData();
     },
     onError: () => {
       genericErrorNotification();
