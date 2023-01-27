cbe3e8f2f58c0077db3b37448c0359b718666d79
BoilingSoup <boilingsoup@protonmail.com>
2023-01-27 08:56:23 -0800

    add char count to comment input box

--- frontend/components/details/CommentTextArea.tsx
+++ frontend/components/details/CommentTextArea.tsx
@@ -1,19 +1,32 @@
-import { Button, Paper, Textarea } from "@mantine/core";
+import { Button, Group, Paper, Text, Textarea } from "@mantine/core";
+import { BaseSyntheticEvent, useRef } from "react";
 import { useAuth } from "../../contexts/AuthProvider";
+import { MAX_COMMENT_LENGTH } from "./constants";
 import { commentsMargin, paperSx } from "./styles";
 import { CommentUserInfo } from "./_commentUserInfo";
 
 export const CommentTextArea = () => {
   const { user } = useAuth();
+  const textAreaRef = useRef<HTMLTextAreaElement>(null);
+  const textRef = useRef<HTMLParagraphElement>(null);
 
   if (!user) {
     return <> </>;
   }
 
+  const textAreaChangeHandler = (event: BaseSyntheticEvent) => {
+    const currCommentLength = event.target.value.length;
+    if (textRef.current !== null) {
+      textRef.current.innerText = `${MAX_COMMENT_LENGTH - currCommentLength}/${MAX_COMMENT_LENGTH}`;
+    }
+  };
+
   return (
     <Paper sx={paperSx} pb={14}>
       <CommentUserInfo avatar={user.avatar} author={user.name} />
       <Textarea
+        ref={textAreaRef}
+        onChange={textAreaChangeHandler}
         size="md"
         minRows={4}
         mx={commentsMargin}
@@ -22,9 +35,14 @@ export const CommentTextArea = () => {
         aria-label="Your comment"
         withAsterisk
       />
-      <Button display="block" px={40} ml="auto" mr={commentsMargin}>
-        Submit
-      </Button>
+      <Group ml={commentsMargin}>
+        <Text ml={commentsMargin} component="p" ref={textRef}>
+          {`${MAX_COMMENT_LENGTH}/${MAX_COMMENT_LENGTH}`}
+        </Text>
+        <Button display="block" px={40} ml="auto" mr={commentsMargin}>
+          Submit
+        </Button>
+      </Group>
     </Paper>
   );
 };
