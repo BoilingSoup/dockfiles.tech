56719f6d235c1014df91f30926f358db40c0ff30
BoilingSoup <boilingsoup@protonmail.com>
2023-02-11 14:37:46 -0800

    ide-helper generate

--- backend/src/_ide_helper_models.php
+++ backend/src/_ide_helper_models.php
@@ -15,7 +15,7 @@ namespace App\Models{
  * App\Models\Bookmarks
  *
  * @property int $id
- * @property int $configuration_id
+ * @property int $environment_id
  * @property int $user_id
  * @property \Illuminate\Support\Carbon|null $created_at
  * @property \Illuminate\Support\Carbon|null $updated_at
@@ -23,8 +23,8 @@ namespace App\Models{
  * @method static \Illuminate\Database\Eloquent\Builder|Bookmarks newModelQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|Bookmarks newQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|Bookmarks query()
- * @method static \Illuminate\Database\Eloquent\Builder|Bookmarks whereConfigurationId($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Bookmarks whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Bookmarks whereEnvironmentId($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Bookmarks whereId($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Bookmarks whereUpdatedAt($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Bookmarks whereUserId($value)
@@ -40,6 +40,8 @@ namespace App\Models{
  * @property string $name
  * @property \Illuminate\Support\Carbon|null $created_at
  * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Models\Environments[] $environments
+ * @property-read int|null $environments_count
  * @method static \Database\Factories\CategoriesFactory factory(...$parameters)
  * @method static \Illuminate\Database\Eloquent\Builder|Categories newModelQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|Categories newQuery()
@@ -59,16 +61,20 @@ namespace App\Models{
  * @property int $id
  * @property string $content
  * @property int $user_id
- * @property int $configuration_id
+ * @property int $environment_id
  * @property \Illuminate\Support\Carbon|null $created_at
  * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \App\Models\User|null $author
+ * @property-read \App\Models\Environments|null $environment
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Models\Replies[] $replies
+ * @property-read int|null $replies_count
  * @method static \Database\Factories\CommentsFactory factory(...$parameters)
  * @method static \Illuminate\Database\Eloquent\Builder|Comments newModelQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|Comments newQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|Comments query()
- * @method static \Illuminate\Database\Eloquent\Builder|Comments whereConfigurationId($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Comments whereContent($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Comments whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Comments whereEnvironmentId($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Comments whereId($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Comments whereUpdatedAt($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Comments whereUserId($value)
@@ -78,52 +84,42 @@ namespace App\Models{
 
 namespace App\Models{
 /**
- * App\Models\Configurations
- *
- * @property int $id
- * @property string $name
- * @property string $description
- * @property string $github_link
- * @property string $direct_link
- * @property int $environment_id
- * @property int $user_id
- * @property \Illuminate\Support\Carbon|null $created_at
- * @property \Illuminate\Support\Carbon|null $updated_at
- * @method static \Database\Factories\ConfigurationsFactory factory(...$parameters)
- * @method static \Illuminate\Database\Eloquent\Builder|Configurations newModelQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|Configurations newQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|Configurations query()
- * @method static \Illuminate\Database\Eloquent\Builder|Configurations whereCreatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Configurations whereDescription($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Configurations whereDirectLink($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Configurations whereEnvironmentId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Configurations whereGithubLink($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Configurations whereId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Configurations whereName($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Configurations whereUpdatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Configurations whereUserId($value)
- */
-	class Configurations extends \Eloquent {}
-}
-
-namespace App\Models{
-/**
  * App\Models\Environments
  *
  * @property int $id
  * @property string $name
+ * @property string $description
+ * @property string $string_id
+ * @property string $repo_owner
+ * @property string $repo_name
+ * @property string $repo_branch
  * @property int $category_id
+ * @property int $user_id
  * @property \Illuminate\Support\Carbon|null $created_at
  * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Models\User[] $bookmarkedBy
+ * @property-read int|null $bookmarked_by_count
+ * @property-read \App\Models\Categories|null $category
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Models\Comments[] $comments
+ * @property-read int|null $comments_count
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Models\User[] $likes
+ * @property-read int|null $likes_count
+ * @property-read \App\Models\User|null $user
  * @method static \Database\Factories\EnvironmentsFactory factory(...$parameters)
  * @method static \Illuminate\Database\Eloquent\Builder|Environments newModelQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|Environments newQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|Environments query()
  * @method static \Illuminate\Database\Eloquent\Builder|Environments whereCategoryId($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Environments whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Environments whereDescription($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Environments whereId($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Environments whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Environments whereRepoBranch($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Environments whereRepoName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Environments whereRepoOwner($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Environments whereStringId($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Environments whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Environments whereUserId($value)
  */
 	class Environments extends \Eloquent {}
 }
@@ -133,7 +129,7 @@ namespace App\Models{
  * App\Models\Likes
  *
  * @property int $id
- * @property int $configuration_id
+ * @property int $environment_id
  * @property int $user_id
  * @property \Illuminate\Support\Carbon|null $created_at
  * @property \Illuminate\Support\Carbon|null $updated_at
@@ -141,8 +137,8 @@ namespace App\Models{
  * @method static \Illuminate\Database\Eloquent\Builder|Likes newModelQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|Likes newQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|Likes query()
- * @method static \Illuminate\Database\Eloquent\Builder|Likes whereConfigurationId($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Likes whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Likes whereEnvironmentId($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Likes whereId($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Likes whereUpdatedAt($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Likes whereUserId($value)
@@ -156,12 +152,15 @@ namespace App\Models{
  *
  * @property int $id
  * @property string $content
- * @property int $is_read
+ * @property bool $is_read
  * @property int $author_id
  * @property int $recipient_id
  * @property int $comment_id
  * @property \Illuminate\Support\Carbon|null $created_at
  * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \App\Models\User|null $author
+ * @property-read \App\Models\Comments|null $comment
+ * @property-read \App\Models\User|null $recipient
  * @method static \Database\Factories\RepliesFactory factory(...$parameters)
  * @method static \Illuminate\Database\Eloquent\Builder|Replies newModelQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|Replies newQuery()
@@ -188,19 +187,31 @@ namespace App\Models{
  * @property \Illuminate\Support\Carbon|null $email_verified_at
  * @property string|null $password
  * @property string|null $avatar
- * @property int $is_admin
+ * @property bool $is_admin
  * @property int $is_banned
- * @property string|null $github_id
+ * @property int|null $github_id
  * @property string|null $github_token
  * @property string|null $github_refresh_token
- * @property string|null $gitlab_id
+ * @property int|null $gitlab_id
  * @property string|null $gitlab_token
  * @property string|null $gitlab_refresh_token
  * @property string|null $remember_token
  * @property \Illuminate\Support\Carbon|null $created_at
  * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Models\Environments[] $bookmarks
+ * @property-read int|null $bookmarks_count
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Models\Comments[] $comments
+ * @property-read int|null $comments_count
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Models\Environments[] $environments
+ * @property-read int|null $environments_count
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Models\Environments[] $likes
+ * @property-read int|null $likes_count
  * @property-read \Illuminate\Notifications\DatabaseNotificationCollection|\Illuminate\Notifications\DatabaseNotification[] $notifications
  * @property-read int|null $notifications_count
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Models\Replies[] $repliesAuthored
+ * @property-read int|null $replies_authored_count
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Models\Replies[] $repliesReceived
+ * @property-read int|null $replies_received_count
  * @property-read \Illuminate\Database\Eloquent\Collection|\Laravel\Sanctum\PersonalAccessToken[] $tokens
  * @property-read int|null $tokens_count
  * @method static \Database\Factories\UserFactory factory(...$parameters)
@@ -226,6 +237,6 @@ namespace App\Models{
  * @method static \Illuminate\Database\Eloquent\Builder|User whereRememberToken($value)
  * @method static \Illuminate\Database\Eloquent\Builder|User whereUpdatedAt($value)
  */
-	class User extends \Eloquent {}
+	class User extends \Eloquent implements \Illuminate\Contracts\Auth\MustVerifyEmail {}
 }
 
