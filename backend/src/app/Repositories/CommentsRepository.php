2bb3c7a3f1d1bd682c3f7ec71f8892fa33858bd7
BoilingSoup <boilingsoup@protonmail.com>
2023-02-25 10:39:13 -0800

    select only id, name, avatar, is_admin fields of a comment/reply author

--- backend/src/app/Repositories/CommentsRepository.php
+++ backend/src/app/Repositories/CommentsRepository.php
@@ -14,72 +14,72 @@ use Illuminate\Support\Facades\Cache;
 
 class CommentsRepository
 {
-    /**
-     * Retrieve cursor paginated list of Environment Comments data and Replies count by string_id.
-     *
-     * @return array
-     */
-    public function index(Request $request)
-    {
-        $stringId = $request->string_id;
-        $cursor = $request->cursor;
+  /**
+   * Retrieve cursor paginated list of Environment Comments data and Replies count by string_id.
+   *
+   * @return array
+   */
+  public function index(Request $request)
+  {
+    $stringId = $request->string_id;
+    $cursor = $request->cursor;
 
-        return Cache::tags([CACHE_TAGS::COMMENTS, CACHE_TAGS::COMMENTS_INDEX])->rememberForever(
-            CACHE_KEYS::ENVIRONMENTS_COMMENTS_($stringId, $cursor),
-            function () use ($stringId) {
-                $environmentId = Environments::where('string_id', $stringId)->firstOrFail()->id;
-
-                return Comments::where(ForeignKeyCol::environments, $environmentId)
-                  ->with('author')
-                  ->withCount('replies')
-                  ->orderByDesc('created_at')
-                  ->cursorPaginate(perPage: 10);
-            }
-        );
-    }
-
-    /**
-     * Retrieve a count of an Environment's Comments by string_id.
-     *
-     * @return int
-     */
-    public function count(Request $request)
-    {
-        $stringId = $request->string_id;
-
-        return Cache::tags([CACHE_TAGS::COMMENTS, CACHE_TAGS::COMMENTS_COUNT])->rememberForever(
-            CACHE_KEYS::ENVIRONMENTS_COMMENTS_COUNT_($stringId),
-            function () use ($stringId) {
-                $environmentId = Environments::where('string_id', $stringId)->firstOrFail()->id;
-
-                return Comments::where(ForeignKeyCol::environments, $environmentId)->count();
-            }
-        );
-    }
-
-    /**
-     * Store a Comment for the Authenticated User with a given Environment string ID.
-     *
-     * @return Comments
-     */
-    public function store(StoreCommentsRequest $request)
-    {
-        $validated = (array) $request->validated();
-        $content = $validated['content'];
-
-        $stringId = $request->string_id;
+    return Cache::tags([CACHE_TAGS::COMMENTS, CACHE_TAGS::COMMENTS_INDEX])->rememberForever(
+      CACHE_KEYS::ENVIRONMENTS_COMMENTS_($stringId, $cursor),
+      function () use ($stringId) {
         $environmentId = Environments::where('string_id', $stringId)->firstOrFail()->id;
 
-        $comment = Comments::create([
-            'content' => $content,
-            ForeignKeyCol::users => Auth::user()->id,
-            ForeignKeyCol::environments => $environmentId,
-        ]);
+        return Comments::where(ForeignKeyCol::environments, $environmentId)
+          ->with('author:id,name,avatar,is_admin')
+          ->withCount('replies')
+          ->orderByDesc('created_at')
+          ->cursorPaginate(perPage: 10);
+      }
+    );
+  }
 
-        Cache::tags([CACHE_TAGS::COMMENTS])->flush();
-        Cache::tags([CACHE_TAGS::ENVIRONMENTS])->flush();
-        Cache::tags([CACHE_TAGS::CATEGORIES])->flush();
+  /**
+   * Retrieve a count of an Environment's Comments by string_id.
+   *
+   * @return int
+   */
+  public function count(Request $request)
+  {
+    $stringId = $request->string_id;
 
-        return $comment;
-    }
+    return Cache::tags([CACHE_TAGS::COMMENTS, CACHE_TAGS::COMMENTS_COUNT])->rememberForever(
+      CACHE_KEYS::ENVIRONMENTS_COMMENTS_COUNT_($stringId),
+      function () use ($stringId) {
+        $environmentId = Environments::where('string_id', $stringId)->firstOrFail()->id;
+
+        return Comments::where(ForeignKeyCol::environments, $environmentId)->count();
+      }
+    );
+  }
+
+  /**
+   * Store a Comment for the Authenticated User with a given Environment string ID.
+   *
+   * @return Comments
+   */
+  public function store(StoreCommentsRequest $request)
+  {
+    $validated = (array) $request->validated();
+    $content = $validated['content'];
+
+    $stringId = $request->string_id;
+    $environmentId = Environments::where('string_id', $stringId)->firstOrFail()->id;
+
+    $comment = Comments::create([
+      'content' => $content,
+      ForeignKeyCol::users => Auth::user()->id,
+      ForeignKeyCol::environments => $environmentId,
+    ]);
+
+    Cache::tags([CACHE_TAGS::COMMENTS])->flush();
+    Cache::tags([CACHE_TAGS::ENVIRONMENTS])->flush();
+    Cache::tags([CACHE_TAGS::CATEGORIES])->flush();
+
+    return $comment;
+  }
 }
--- backend/src/app/Repositories/RepliesRepository.php
+++ backend/src/app/Repositories/RepliesRepository.php
@@ -14,49 +14,50 @@ use Illuminate\Support\Facades\Cache;
 
 class RepliesRepository
 {
-    /**
-     * Get a paginated list of Replies for a specific Comment.
-     *
-     * @return Replies
-     */
-    public function index(Request $request)
-    {
-        $commentId = $request->id;
-        $cursor = $request->cursor;
+  /**
+   * Get a paginated list of Replies for a specific Comment.
+   *
+   * @return Replies
+   */
+  public function index(Request $request)
+  {
+    $commentId = $request->id;
+    $cursor = $request->cursor;
 
-        return Cache::tags([CACHE_TAGS::REPLIES_($commentId)])->rememberForever(
-            CACHE_KEYS::REPLIES_($commentId, $cursor),
-            function () use ($commentId) {
-                return Replies::where(ForeignKeyCol::comments, $commentId)
-                  ->orderBy('created_at')
-                  ->cursorPaginate(perPage: 5);
-            }
-        );
-    }
+    return Cache::tags([CACHE_TAGS::REPLIES_($commentId)])->rememberForever(
+      CACHE_KEYS::REPLIES_($commentId, $cursor),
+      function () use ($commentId) {
+        return Replies::where(ForeignKeyCol::comments, $commentId)
+          ->with('author:id,name,avatar,is_admin')
+          ->orderBy('created_at')
+          ->cursorPaginate(perPage: 5);
+      }
+    );
+  }
 
-    /**
-     * Store a Reply to a Comment in the database.
-     *
-     * @return Replies
-     */
-    public function store(StoreRepliesRequest $request)
-    {
-        $validated = (array) $request->validated();
-        $content = $validated['content'];
+  /**
+   * Store a Reply to a Comment in the database.
+   *
+   * @return Replies
+   */
+  public function store(StoreRepliesRequest $request)
+  {
+    $validated = (array) $request->validated();
+    $content = $validated['content'];
 
-        $commentId = $request->id;
-        $comment = Comments::findOrFail($commentId);
+    $commentId = $request->id;
+    $comment = Comments::findOrFail($commentId);
 
-        $reply = Replies::create([
-            'content' => $content,
-            ForeignKeyCol::reply_author => Auth::user()->id,
-            ForeignKeyCol::reply_recipient => $comment[ForeignKeyCol::users],
-            ForeignKeyCol::comments => $comment->id,
-        ]);
+    $reply = Replies::create([
+      'content' => $content,
+      ForeignKeyCol::reply_author => Auth::user()->id,
+      ForeignKeyCol::reply_recipient => $comment[ForeignKeyCol::users],
+      ForeignKeyCol::comments => $comment->id,
+    ]);
 
-        Cache::tags([CACHE_TAGS::COMMENTS])->flush();
-        Cache::tags([CACHE_TAGS::REPLIES_($commentId)])->flush();
+    Cache::tags([CACHE_TAGS::COMMENTS])->flush();
+    Cache::tags([CACHE_TAGS::REPLIES_($commentId)])->flush();
 
-        return $reply;
-    }
+    return $reply;
+  }
 }
