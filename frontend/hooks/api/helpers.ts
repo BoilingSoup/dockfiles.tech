109e132b7b6f74c22af89493ca422a738b3d579a
BoilingSoup <boilingsoup@protonmail.com>
2023-02-25 14:02:52 -0800

    update replies page type definition

--- frontend/hooks/api/helpers.ts
+++ frontend/hooks/api/helpers.ts
@@ -194,13 +194,19 @@ export type RepliesData = {
 export type RepliesPage = {
   success: boolean;
   data: {
+    current_page: number;
     data: RepliesData[];
+    first_page_url: string;
+    from: number;
+    last_page: number;
+    last_page_url: string;
+    links: Array<{ url: string | null; label: string; active: boolean }>;
+    next_page_url: string | null;
     path: string;
     per_page: number;
-    next_cursor: string | null;
-    next_page_url: string | null;
-    prev_cursor: string | null;
     prev_page_url: string | null;
+    to: number;
+    total: number;
   };
 };
 
