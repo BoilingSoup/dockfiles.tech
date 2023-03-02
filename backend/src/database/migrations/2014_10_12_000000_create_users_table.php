a996e649db73d1e3bb8aa88c6b4eb7d61ff12ff1
BoilingSoup <boilingsoup@protonmail.com>
2023-03-02 08:32:43 -0800

    fix test

--- backend/src/database/migrations/2014_10_12_000000_create_users_table.php
+++ backend/src/database/migrations/2014_10_12_000000_create_users_table.php
@@ -1,6 +1,5 @@
 <?php
 
-use Database\Helpers\MaxLength;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -16,7 +15,7 @@ return new class() extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name', MaxLength::name);
+            $table->string('name');
             $table->string('email');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password')->nullable();
--- backend/src/database/migrations/2022_11_02_193356_create_replies_table.php
+++ backend/src/database/migrations/2022_11_02_193356_create_replies_table.php
@@ -1,7 +1,6 @@
 <?php
 
 use Database\Helpers\ForeignKeyCol;
-use Database\Helpers\MaxLength;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -17,7 +16,7 @@ return new class() extends Migration
     {
         Schema::create('replies', function (Blueprint $table) {
             $table->id();
-            $table->string('content', MaxLength::comments + MaxLength::name + 2); // comment content + name + @ + space
+            $table->string('content');
             $table->boolean('is_read')->default(false);
             $table->foreignId(ForeignKeyCol::reply_author)->index(); // PlanetScale doesn't allow foreign key constraints. Enforce data consistency at app-level.
             $table->foreignId(ForeignKeyCol::reply_recipient)->index();
